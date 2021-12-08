d0b032693022a10d2136fe9d0496ceed4072ca0d
kondrei <kondrei@gmail.com>
2021-12-08 20:24:13 +0200

    Create for with index

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 const Counter = {
-	data() {
-		return {
-			counter: 0,
-			message: "",
-			seen: true,
-			lista: []
-		};
-	},
-	methods: {
-		addToList() {
-			if (this.message === "") return;
-			if (this.lista[this.lista.length - 1] !== this.message) {
-				this.lista.push(this.message);
-				this.message = "";
-			}
-		}
-	},
-	computed: {
-		totalList() {
-			let values = {
-				number: this.lista.length,
-				text: "",
-				class: ""
-			};
+  data() {
+    return {
+      counter: 0,
+      message: "",
+      seen: true,
+      lista: []
+    };
+  },
+  methods: {
+    addToList() {
+      if (this.message === "") return;
+      if (this.lista[this.lista.length - 1] !== this.message) {
+        this.lista.push(this.message);
+        this.message = "";
+      }
+    }
+  },
+  computed: {
+    totalList() {
+      let values = {
+        number: this.lista.length,
+        text: "",
+        class: ""
+      };
 
-			if (values.number <= 0) {
-				values.text = "No elements";
-				values.class = "error";
-			}
-			values.text =
-				values.number + (values.number === 1 ? " element" : " elements");
+      if (values.number <= 0) {
+        values.text = "No elements.";
+        values.class = "error";
+      }
+      values.text =
+        values.number + (values.number === 1 ? " element" : " elements");
 
-			return values;
-		}
-	},
-	mounted() {
-		console.log("app loadet");
-	}
+      return values;
+    }
+  },
+  mounted() {
+    console.log("app loadet");
+  }
 };
 
 const app = Vue.createApp(Counter);
