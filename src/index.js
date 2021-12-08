0ecbcfdd52f06280970666934311d87afa01a365
kondrei <kondrei@gmail.com>
2021-12-08 09:38:15 +0200

    learn by doing

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 const Counter = {
-  data() {
-    return {
-      counter: 0,
-      message: "",
-      seen: true,
-      lista: ["ceva", "altceva", "si nimic"]
-    };
-  },
-  mounted() {
-    setInterval(() => {
-      this.counter++;
-    }, 1000);
-  },
-  created() {
-    console.log(this.seen);
-  },
-  methods: {
-    addToList() {
-      if (this.message === "") return;
-      if (this.lista[this.lista.length - 1] !== this.message) {
-        this.lista.push(this.message);
-        this.message = "";
-      }
-    }
-  }
+	data() {
+		return {
+			counter: 0,
+			message: "",
+			seen: true,
+			lista: []
+		};
+	},
+	mounted() {
+		setInterval(() => {
+			this.counter++;
+		}, 1000);
+	},
+	created() {
+		console.log(this.seen);
+	},
+	methods: {
+		addToList() {
+			if (this.message === "") return;
+			if (this.lista[this.lista.length - 1] !== this.message) {
+				this.lista.push(this.message);
+				this.message = "";
+			}
+		}
+	},
+	computed: {
+		totalList() {
+			let total = this.lista.length;
+			if (total <= 0) return "No elements";
+			return total + (total === 1 ? " element" : " elements");
+		}
+	}
 };
 
 const app = Vue.createApp(Counter);
 const viewModel = app.mount("#counter");
-viewModel.lista.push("ceva");
