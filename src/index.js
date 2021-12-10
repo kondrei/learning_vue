d3dcb8694ada4224ae0ddca52d90eb6f465c35ec
kondrei <kondrei@gmail.com>
2021-12-10 18:02:19 +0200

    saving work

--- src/index.js
+++ src/index.js
@@ -1,56 +1,63 @@
 const Counter = {
-	data() {
-		return {
-			myBooks: ["roata timpului", "dune"],
-			counter: 0,
-			message: "",
-			seen: true
-		};
-	},
-	methods: {
-		addToList() {
-			if (this.message === "") return;
-			if (this.myBooks[this.myBooks.length - 1] !== this.message) {
-				this.myBooks.push(this.message);
-				console.log(viewModel.myBooks);
-				this.message = "";
-			}
-		}
-	},
-	computed: {
-		totalList() {
-			let values = {
-				number: this.myBooks.length,
-				text: "",
-				class: ""
-			};
+  data() {
+    return {
+      myBooks: ["roata timpului", "dune"],
+      counter: 0,
+      message: "",
+      seen: true
+    };
+  },
+  methods: {
+    addToList() {
+      if (this.message === "") return;
+      if (this.myBooks[this.myBooks.length - 1] !== this.message) {
+        this.myBooks.push(this.message);
+        console.log(this.message);
+        this.message = "";
+      }
+    },
+    write(message, event) {
+      console.log(message);
 
-			if (values.number <= 0) {
-				values.text = "No elements.";
-				values.class = "error";
-			}
-			values.text =
-				values.number + (values.number === 1 ? " element" : " elements");
+      if (event) {
+        event.preventDefault();
+      }
+    }
+  },
+  computed: {
+    totalList() {
+      let values = {
+        number: this.myBooks.length,
+        text: "",
+        class: ""
+      };
 
-			return values;
-		}
-	},
-	mounted() {
-		console.log("app loadet");
-	}
+      if (values.number <= 0) {
+        values.text = "No elements.";
+        values.class = "error";
+      }
+      values.text =
+        values.number + (values.number === 1 ? " element" : " elements");
+
+      return values;
+    }
+  },
+  mounted() {
+    console.log("app loadet at " + new Date().toLocaleTimeString());
+  }
 };
 
 const app = Vue.createApp(Counter);
 
 app.component("books-list", {
-	template: `
+  template: `
     <li>  
       {{ books }}
       <button v-on:click="$emit('remove')">Remove</button>
     </li>
   `,
-	props: ["books"],
-	emits: ["remove"]
+  props: ["books"],
+  emits: ["remove"]
 });
 
 const viewModel = app.mount("#counter");
