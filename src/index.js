e3d401a15e7f37eaf95658e791b573172e6efd9c
kondrei <kondrei@gmail.com>
2021-12-10 10:26:36 +0200

    todo list

--- src/index.js
+++ src/index.js
@@ -1,43 +1,55 @@
 const Counter = {
-  data() {
-    return {
-      myBooks: [{ id: 1, title: "Roata timpului" }],
-      counter: 0,
-      message: "",
-      seen: true
-    };
-  },
-  methods: {
-    addToList() {
-      if (this.message === "") return;
-      if (this.myBooks[this.myBooks.length - 1] !== this.message) {
-        this.myBooks.push(this.message);
-        this.message = "";
-      }
-    }
-  },
-  computed: {
-    totalList() {
-      let values = {
-        number: this.myBooks.length,
-        text: "",
-        class: ""
-      };
+	data() {
+		return {
+			myBooks: [],
+			counter: 0,
+			message: "",
+			seen: true
+		};
+	},
+	methods: {
+		addToList() {
+			if (this.message === "") return;
+			if (this.myBooks[this.myBooks.length - 1] !== this.message) {
+				this.myBooks.push(this.message);
+				console.log(viewModel.myBooks);
+				this.message = "";
+			}
+		}
+	},
+	computed: {
+		totalList() {
+			let values = {
+				number: this.myBooks.length,
+				text: "",
+				class: ""
+			};
 
-      if (values.number <= 0) {
-        values.text = "No elements.";
-        values.class = "error";
-      }
-      values.text =
-        values.number + (values.number === 1 ? " element" : " elements");
+			if (values.number <= 0) {
+				values.text = "No elements.";
+				values.class = "error";
+			}
+			values.text =
+				values.number + (values.number === 1 ? " element" : " elements");
 
-      return values;
-    }
-  },
-  mounted() {
-    console.log("app loadet");
-  }
+			return values;
+		}
+	},
+	mounted() {
+		console.log("app loadet");
+	}
 };
 
 const app = Vue.createApp(Counter);
 const viewModel = app.mount("#counter");
+
+app.component("books-list", {
+	template: `
+    <li>  
+      {{books}}
+      <button @click="$emit('remove')">Remove</button>
+    </li>
+  `,
+	props: ["books"],
+	emits: ["remove"]
+});
