98907a88dab710d1193605c34eef873323c2a427
kondrei <kondrei@gmail.com>
2021-12-27 14:52:31 +0200

    learning vue

--- src/app.js
+++ src/app.js
@@ -4,12 +4,25 @@ const app = Vue.createApp({
             title: 'The final empire',
             author: 'Brandon',
             age: 41,
-            show: true
+            show: true,
+            x: 0,
+            y: 0,
+            books: [
+                { title: 'man in the castle', author: 'patrik', image: 'assets/1.jpg', isFav: true },
+                { title: 'ord of the rings', author: 'tolkien', image: 'assets/2.jpg', isFav: false },
+                { title: 'cel mai iubit dintre pamanteni', author: 'eliade', image: 'assets/3.jpg', isFav: true }
+            ],
+            url: 'http://google.com'
         }
     },
     methods: {
-        toggleShow() {
-            this.show = !this.show
+        markFav(fav) {
+            this.books[fav].isFav = !this.books[fav].isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
